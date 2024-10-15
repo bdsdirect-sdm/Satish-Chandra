c35a58850b61c318a51f3138968660171cc2b2a0
bdsdirect-sdm <bdsdirect.sdm@smartdatainc.net>
2024-10-15 17:44:43 +0530

    Delete backend directory

--- backend/src/database.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('testing', 'root', 'Password123#@!', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-export default sequelize;
